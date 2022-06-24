a41b786d6a45c573d9c370e1882e2766b7a37e14
Dmitry Savonin <dmitry@ankr.com>
2022-06-24 16:25:16 +0300

    added migrations config

--- migrations/2_relay_hub.js
+++ migrations/2_relay_hub.js
@@ -4,9 +4,21 @@ const SimpleTokenFactory = artifacts.require("SimpleTokenFactory");
 const BridgeRouter = artifacts.require("BridgeRouter");
 const CrossChainBridge = artifacts.require("CrossChainBridge");
 
+const BLOCKCHAIN_CONFIG = {
+  'smartchain': {
+    blockConfirmations: 12,
+    epochLength: 200,
+    nativeTokenSymbol: 'BNB',
+    nativeTokenName: 'BNB',
+  },
+};
+
 module.exports = async (deployer) => {
+  const config = BLOCKCHAIN_CONFIG[deployer.network];
+  if (!config) throw new Error(`There is no config for network: ${deployer.network}`)
+  const {blockConfirmations, epochLength, nativeTokenSymbol, nativeTokenName} = config;
   // deploy parlia block verifier as default verifier
-  await deployer.deploy(ParliaBlockVerifier, 12, 200)
+  await deployer.deploy(ParliaBlockVerifier, blockConfirmations, epochLength)
   const parliaBlockVerifier = await ParliaBlockVerifier.deployed();
   // deploy relay hub
   await deployer.deploy(RelayHub, parliaBlockVerifier.address);
@@ -21,5 +33,5 @@ module.exports = async (deployer) => {
   await deployer.deploy(CrossChainBridge);
   const crossChainBridge = await CrossChainBridge.deployed();
   // inti cross bridge
-  await crossChainBridge.initialize(relayHub.address, relayHub.address, simpleTokenFactory.address, bridgeRouter.address, "BNB", "BNB");
+  await crossChainBridge.initialize(relayHub.address, relayHub.address, simpleTokenFactory.address, bridgeRouter.address, nativeTokenSymbol, nativeTokenName);
 };
